292bacba1a6e74810771589c36bae6903e532ffc
pank4s29-oss <pank4s29@gmail.com>
2026-08-20 12:10:47 +0800

    Update pain-points-hub.js

--- api/pain-points-hub.js
+++ api/pain-points-hub.js
@@ -1,5 +1,5 @@
-const { getUserFromRequest, restRequest, sendError } = require('../_lib/supabase');
-const { call, parseJSON } = require('../_lib/provider');
+const { getUserFromRequest, restRequest, sendError } = require('./_lib/supabase');
+const { call, parseJSON } = require('./_lib/provider');
 
 // 這支合併了原本 3 支獨立檔案：
 //   POST /api/domain-profiles/:id/pain-points          (action 未帶)
